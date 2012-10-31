d899ee09837661f4050e3f0a953f7d8ee792e79f
Ryan Corradini <ryan@buyog.com>
2012-10-31 00:23:22 -0400

    Changed Tao to a class
    Moved previous function behavior to Tao.zen(), added Tao.create()

--- tao.js
+++ tao.js
@@ -11,7 +11,7 @@
 /*
 Example, from the Zen Coding homepage:
 
-var dom_fragment = zen("div#page>div.logo+ul#navigation>li*5>a");
+var dom_fragment = tao.zen("div#page>div.logo+ul#navigation>li*5>a");
 
 dom_fragment.toString():
 
@@ -34,8 +34,8 @@ define(
 
         var _tagRex   = /^[a-z]+[1-6]?/i,
             _posRex   = /[>|<|\+]+/g,
-            _idRex    = /#[a-z]+/i,
-            _classRex = /\.[a-z]+/i,
+            _idRex    = /#[_a-z]+[_a-z0-9-]*/i,
+            _classRex = /\.-?[_a-z]+[_a-z0-9-]*/i,                ///\.[a-z]+/i,
             _textRex  = /{[^}]+}/,
             _countRex = /\*[0-9]+/;
 
@@ -72,17 +72,19 @@ define(
             return el;
         }
 
-        // here's the to-be-exposed function
-        function _parseTree(exp, serialize) {
+
+    // here are the to-be-exposed functions
+
+        function _parseTree(expression, serialize) {
             var _frag = (serialize) ? document.createElement('div') : document.createDocumentFragment(),
                 _cur = _frag,
                 child = null,
-                i, posCode, tags = exp.split(_posRex);
+                i, posCode, tags = expression.split(_posRex);
             for (i=0; i<tags.length; i++) {
                 child = _buildElement(tags[i]);
                 if (child) {
                     _cur.appendChild(child);
-                    posCode = _posRex.exec(exp);
+                    posCode = _posRex.exec(expression);
 
                     if (posCode && posCode.length) {
                         switch (posCode[0]) {
@@ -108,10 +110,16 @@ define(
             return (serialize) ? _frag.innerHTML : _frag;
         }
 
+        function _createElement(expression) {
+            var tags = expression.split(_posRex);
+            return _buildElement(tags[0]);
+        }
 
-        // return Tao generator function
-        return function tao(expression, serialize) {
-            return _parseTree(expression, serialize);
+
+        // return available generator functions
+        return {
+            zen    : _parseTree,
+            create : _createElement
         };
     }
 );
